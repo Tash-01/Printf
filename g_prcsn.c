13d2c24cd8d2178c7e4d010f1fc51869e20488fe
PizzasaurusR <ramizissel@gmail.com>
2023-04-23 14:30:45 -0700

    Create g_prcsn.c

---
+++ g_prcsn.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * g_prcsn - prcsn calc for print
+ * @format: str to print to
+ * @i: argument list to print.
+ * @list: argument list.
+ *
+ * Return: prcsn.
+ */
+int g_prcsn(const char *format, int *x, va_list list)
+{
+	int index_pos = *x + 1;
+	int prcsn = -1;
+
+	if (format[index_pos] != '.')
+		return (prcsn);
+
+	prcsn = 0;
+
+	for (index_pos += 1; format[index_pos] != '\0'; index_pos++)
+	{
+		if (is_digit(format[index_pos]))
+		{
+			prcsn *= 10;
+			prcsn += format[index_pos] - '0';
+		}
+		else if (format[index_pos] == '*')
+		{
+			index_pos++;
+			prcsn = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
+
+	*i = index_pos - 1;
+
+	return (prcsn);
+}
